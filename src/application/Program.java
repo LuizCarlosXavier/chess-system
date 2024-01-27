ea57f9e95736d1a3fd816429c06f336b5782246f
LuizCarlosXavier <luizcarlosabrantes01@gmail.com>
2024-01-27 00:51:52 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
+    public static void main(String[] args) {
+        
+        Position position = new Position(3, 5);
+        System.out.println(position);
+
+    }
     
 }
